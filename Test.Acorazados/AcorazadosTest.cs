fd8030af262ac5475bb3fab5ef4af43ebf774e8e
Michael Andrés Lozano Sánchez <Michael.Lozano@sinco.com.co>
2025-11-18 10:44:26 -0500

    🔴Si_SeInciaUnTableroUnTamaño10_10_Debe_ExistirUnTablero10_10

--- Test.Acorazados/AcorazadosTest.cs
+++ Test.Acorazados/AcorazadosTest.cs
@@ -5,11 +5,21 @@ namespace Test.BattleShip;
 public class AcorazadosTest
 {
     [Fact]
-    public void Si_SeInciaUnTableroUnTamaño00_Debe_MostrarUnaExcepcion()
+    public void Si_SeInciaUnTableroUnTamaño0_0_Debe_MostrarUnaExcepcion()
     {
         var tablero = () => new Tablero(0, 0);
 
         tablero.Should().Throw<ArgumentOutOfRangeException>();
+    }
+    
+    [Fact]
+    public void Si_SeInciaUnTableroUnTamaño10_10_Debe_ExistirUnTablero10_10()
+    {
+        var tablero = new Tablero(10, 10);
+
+        var existeTablero = tablero.Existe(10, 10);
+        
+        existeTablero.Should().BeTrue();
     }
 }
 
@@ -18,5 +28,10 @@ public class Tablero
     public Tablero(int i, int i1)
     {
         throw new ArgumentOutOfRangeException();
+    }
+
+    public bool Existe(int i, int i1)
+    {
+        throw new NotImplementedException();
     }
 }
