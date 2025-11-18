ff5205d2aa274f1f1753d37bb5e54ef2db854ebc
Brandon Amaya Esquivel <brandon.amaya@sinco.com.co>
2025-11-18 11:32:43 -0500

    ✅Si_CreoUnDestructorEnLaPosicion13ConDireccionAbajo_Debe_LasCasillas13_12_11_Tenerd

--- Acorazados/Jugador.cs
+++ Acorazados/Jugador.cs
@@ -43,6 +43,12 @@ public class Jugador
                 Tablero[x-1, y] = "d";
                 Tablero[x-2, y] = "d";
             }
+            else if (direccion.Equals(Direccion.Abajo))
+            {
+                Tablero[x, y] = "d";
+                Tablero[x, y-1] = "d";
+                Tablero[x, y-2] = "d";
+            }
             else
             {
                 Tablero[x, y] = "d";
