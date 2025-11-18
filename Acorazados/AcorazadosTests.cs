f33776d7889a8ddbdd6bf4f5a19b164dd8132c53
Brandon Amaya Esquivel <brandon.amaya@sinco.com.co>
2025-11-18 10:18:58 -0500

    🔴 Si_CreoUnJugador_Debe_TenerUnTableroDe10x10

--- Acorazados/AcorazadosTests.cs
+++ Acorazados/AcorazadosTests.cs
@@ -11,11 +11,22 @@ public class AcorazadosTests
         
         jugador.Nombre.Should().Be("Jugador 1");
     }
+
+    [Fact]
+    public void Si_CreoUnJugador_Debe_TenerUnTableroDe10x10()
+    {
+        var jugador = new Jugador("Jugador 1");
+        
+        jugador.Tablero.GetLength(0).Should().Be(10);
+        jugador.Tablero.GetLength(1).Should().Be(10);
+    }
 }
 
 public class Jugador
 {
     public string Nombre { get; private set; }
+    public string[][] Tablero { get; set; }
+
     public Jugador(string player)
     {
        Nombre = player;
