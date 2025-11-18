7d03b50149eb47469af48932c89d7b389b320da9
Michael Andrés Lozano Sánchez <Michael.Lozano@sinco.com.co>
2025-11-18 10:32:25 -0500

    🟢 Si_SeInciaUnTableroUnTamaño00_Debe_MostrarUnaExcepcion

---
+++ Test.Acorazados/AcorazadosTest.cs
@@ -0,0 +1,22 @@
+﻿using AwesomeAssertions;
+
+namespace Test.BattleShip;
+
+public class AcorazadosTest
+{
+    [Fact]
+    public void Si_SeInciaUnTableroUnTamaño00_Debe_MostrarUnaExcepcion()
+    {
+        var tablero = () => new Tablero(0, 0);
+
+        tablero.Should().Throw<ArgumentOutOfRangeException>();
+    }
+}
+
+public class Tablero
+{
+    public Tablero(int i, int i1)
+    {
+        throw new ArgumentOutOfRangeException();
+    }
+}
