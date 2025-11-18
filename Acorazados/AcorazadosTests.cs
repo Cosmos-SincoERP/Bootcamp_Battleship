ab19dbdb6dcd20598f3953412fd67724fe45f978
Brandon Amaya Esquivel <brandon.amaya@sinco.com.co>
2025-11-18 12:24:24 -0500

    🔴Si_CreoUnDestructorEnLaPosicion00ConDireccionIzquierda_Debe_LanzarUnaExcepcionDeFueraDeRango

--- Acorazados/AcorazadosTests.cs
+++ Acorazados/AcorazadosTests.cs
@@ -118,6 +118,12 @@ public class AcorazadosTests
         Action act = ()=> jugador.AgregarAcorazado(Acorazado.Destructor, 9, 9, Direccion.Abajo);
         act.Should().Throw<ArgumentOutOfRangeException>().WithMessage("*No es posible ubicar el acorazado en esa direccion*");
     }
-    
-    
+
+    [Fact]
+    public void Si_CreoUnDestructorEnLaPosicion00ConDireccionIzquierda_Debe_LanzarUnaExcepcionDeFueraDeRango()
+    {
+        Action act = () => jugador.AgregarAcorazado(Acorazado.Destructor, 0, 0, Direccion.Izquierda);
+        
+        act.Should().Throw<ArgumentOutOfRangeException>().WithMessage("*No es posible ubicar el acorazado en esa direccion*");
+    }
 }
