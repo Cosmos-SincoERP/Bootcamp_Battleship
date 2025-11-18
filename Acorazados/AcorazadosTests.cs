70e9b7e81b1d082977483e465b73b29d24a13a6d
Brandon Amaya Esquivel <brandon.amaya@sinco.com.co>
2025-11-18 10:12:32 -0500

    🔴 Si_CreoUnJugador1_Debe_JugadorNombreSerJugador1

---
+++ Acorazados/AcorazadosTests.cs
@@ -0,0 +1,24 @@
+﻿using AwesomeAssertions;
+
+namespace Acorazados;
+
+public class AcorazadosTests
+{
+    [Fact]
+    public void Si_CreoUnJugador1_Debe_JugadorNombreSerJugador1()
+    {
+        var jugador = new Jugador("Jugador 1");
+        
+        jugador.Nombre.Should().Be("Jugador 1");
+    }
+}
+
+public class Jugador
+{
+    public Jugador(string player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object Nombre { get; set; }
+}
