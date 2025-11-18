3c136c035268a4b89fc718948b2204bd7e31541a
Michael Andrés Lozano Sánchez <Michael.Lozano@sinco.com.co>
2025-11-18 17:01:53 -0500

    🟢 Si_AgregoUnDestructorEnElTablero_De_PodersePosicionar

--- Test.Acorazados/AcorazadosTest.cs
+++ Test.Acorazados/AcorazadosTest.cs
@@ -154,9 +154,10 @@ public class JuegoAcorazado
         }
         else
         {
-            _tablero[2, 2] = "d";
-            _tablero[3, 2] = "d";
-            _tablero[4, 2] = "d";
+            for (int i = 0; i < 3; i++)
+            {
+                _tablero[posicionEnX + i, posicionEnY] = "d";
+            }
         }
     }
 }
