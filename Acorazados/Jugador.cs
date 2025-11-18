0d42bb8b6daead42f7440c2a2c2b32fc0dcbe9d1
Brandon Amaya Esquivel <brandon.amaya@sinco.com.co>
2025-11-18 10:54:13 -0500

    ✅ Si_CreoUnCañoneroEnLaPosicionmenos1menos1_Debe_LanzarUnaExcepcionDeFueraDeRango

--- Acorazados/Jugador.cs
+++ Acorazados/Jugador.cs
@@ -13,6 +13,10 @@ public class Jugador
 
     public void AgregarAcorazado(string tipoBarco, int x, int y)
     {
+        if (x < 0 || y < 0)
+        {
+            throw new ArgumentOutOfRangeException("La posicion no existe en el tablero");
+        }
         if (ObtenerLongitudTablero(0) < x || ObtenerLongitudTablero(1) < y)
         {
             throw new ArgumentOutOfRangeException("La posicion no existe en el tablero");
