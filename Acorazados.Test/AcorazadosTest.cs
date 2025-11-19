4cc81c7b2402653ebe66b40420364ce32b44b9e1
SantiagoRamirezSINCO <santiago.ramirez@sinco.com.co>
2025-11-19 11:39:54 -0500

    🔴 Si_InicioElJuegoConUnDestructorDe_6_1_a_6_3EImprimo_Debe_ImprimirElTableroConUna_d_de_6_1_a_6_3

--- Acorazados.Test/AcorazadosTest.cs
+++ Acorazados.Test/AcorazadosTest.cs
@@ -201,4 +201,33 @@ public class AcorazadosTest
 
         tablero.Should().Be(tableroEsperado);
     }
+    
+    [Fact]
+    public void Si_InicioElJuegoConUnDestructorDe_6_1_a_6_3EImprimo_Debe_ImprimirElTableroConUna_d_de_6_1_a_6_3()
+    {
+        var acorazados = new Battleship();
+
+        acorazados.AddPlayer("Santi");
+        acorazados.AddPlayer("Ruben");
+
+        var destructor = new Nave(6, 1,6,3 , "d");
+
+        acorazados.Iniciar([destructor],[]);
+
+        var tablero = acorazados.Imprimir();
+
+        var tableroEsperado = "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |" +
+                              "0| | | | | | | | | | |" +
+                              "1| | | | | | | | | | |" +
+                              "2| | | | | | | | | | |" +
+                              "3| | | | | | | | | | |" +
+                              "4| | | | | | | | | | |" +
+                              "5| | | | | | | | | | |" +
+                              "6| | d | d | d | | | | | | |" +
+                              "7| | | | | | | | | | |" +
+                              "8| | | | | | | | | | |" +
+                              "9| | | | | | | | | | |";
+
+        tablero.Should().Be(tableroEsperado);
+    }
 }
