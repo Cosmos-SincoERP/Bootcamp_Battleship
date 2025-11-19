1d799bf78776965b1039dd6aa6922e7536fba57e
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-11-19 11:57:38 -0500

    ❤️Si_InicioJuegoAgrego1JugadorYPosiciono3Destructores_Debe_LanzarExcepcion

--- TDDKatas.Test/AcorazadosTests.cs
+++ TDDKatas.Test/AcorazadosTests.cs
@@ -410,4 +410,19 @@ public class AcorazadosTests
         resultado.Should().ThrowExactly<InvalidOperationException>()
             .WithMessage("*No es posible agregar mas de 4 cañoreros");
     }
+
+    [Fact]
+    public void Si_InicioJuegoAgrego1JugadorYPosiciono3Destructores_Debe_LanzarExcepcion()
+    {
+        var acorazado = new Acorazado();
+        acorazado.AgregarJugador("Player 1");
+        acorazado.PosicionarNave(0, 0, TiposNave.Destructor, Orientacion.Derecha);
+        acorazado.PosicionarNave(1, 0, TiposNave.Destructor, Orientacion.Derecha);
+
+        var resultado = () => acorazado.PosicionarNave(2, 0, TiposNave.Destructor, Orientacion.Derecha);
+        
+        resultado.Should().ThrowExactly<InvalidOperationException>()
+            .WithMessage("*No es posible agregar mas de 3 destructores");
+    }
+    
 }
