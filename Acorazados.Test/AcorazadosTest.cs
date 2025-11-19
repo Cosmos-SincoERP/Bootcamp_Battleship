2fd7f277a0b04c35294c3dca3216b05ff13508bb
SantiagoRamirezSINCO <santiago.ramirez@sinco.com.co>
2025-11-19 10:53:31 -0500

    🔴 SiInicioElJuegoConUnCanoneroDeJugador1DebeImprimirloBien

--- Acorazados.Test/AcorazadosTest.cs
+++ Acorazados.Test/AcorazadosTest.cs
@@ -1,4 +1,5 @@
-﻿using AwesomeAssertions;
+﻿using System.Net.NetworkInformation;
+using AwesomeAssertions;
 
 namespace Acorazados.Test;
 
@@ -27,6 +28,42 @@ public class AcorazadosTest
                               "9| | | | | | | | | | |";
 
         tablero.Should().Be(tableroEsperado);
+    }
+
+    [Fact]
+    public void SiInicioElJuegoConUnCanoneroDeJugador1DebeImprimirloBien()
+    {
+        var acorazados = new Battleship();
+
+        acorazados.AddPlayer("Santi");
+        acorazados.AddPlayer("Ruben");
+
+        var canonero = new Nave(1, 1, 1, 1, "g");
+        
+        acorazados.Iniciar([canonero],[]);
+
+        var tablero = acorazados.Imprimir();
+        var tableroEsperado = "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |" +
+                              "0| | | | | | | | | | |" +
+                              "1| | | | | | | | | | |" +
+                              "2| | | | | | | | | | |" +
+                              "3| | | | | | | | | | |" +
+                              "4| | | | | | | | | | |" +
+                              "5| | | | | | | | | | |" +
+                              "6| | | | | | | | | | |" +
+                              "7| | | | | | | | | | |" +
+                              "8| | | | | | | | | | |" +
+                              "9| | | | | | | | | | |";
+
+        tablero.Should().Be(tableroEsperado);
+    }
+}
+
+public class Nave
+{
+    public Nave(int i, int i1, int i2, int i3, string s)
+    {
+        throw new NotImplementedException();
     }
 }
 
