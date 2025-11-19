18b6b1ccb08fecd121626370a1ca449ad77c13b3
SantiagoRamirezSINCO <santiago.ramirez@sinco.com.co>
2025-11-19 10:48:28 -0500

    🔴 TableroDelJugador1DebeSerVacioAlIniciarElJuegoSinNaves

---
+++ Acorazados.Test/AcorazadosTest.cs
@@ -0,0 +1,49 @@
+﻿using AwesomeAssertions;
+
+namespace Acorazados.Test;
+
+public class AcorazadosTest
+{
+    [Fact]
+    public void TableroDelJugador1DebeSerVacioAlIniciarElJuegoSinNaves()
+    {
+        var acorazados = new Battleship();
+
+        acorazados.AddPlayer("Santi");
+        acorazados.AddPlayer("Ruben");
+        acorazados.Iniciar([],[]);
+
+        var tablero = acorazados.Imprimir();
+        var tableroEsperado = "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |" +
+                              "0| | | | | | | | | | |" +
+                              "1| | | | | | | | | | |" +
+                              "2| | | | | | | | | | |" +
+                              "3| | | | | | | | | | |" +
+                              "4| | | | | | | | | | |" +
+                              "5| | | | | | | | | | |" +
+                              "6| | | | | | | | | | |" +
+                              "7| | | | | | | | | | |" +
+                              "8| | | | | | | | | | |" +
+                              "9| | | | | | | | | | |";
+
+        tablero.Should().Be(tableroEsperado);
+    }
+}
+
+public class Battleship
+{
+    public void AddPlayer(string santi)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Iniciar(List<object> objects, List<object> list)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object Imprimir()
+    {
+        throw new NotImplementedException();
+    }
+}
