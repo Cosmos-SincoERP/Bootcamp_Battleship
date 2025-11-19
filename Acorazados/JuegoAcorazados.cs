605981e11f0baa96b71b346f7deb2a5ac8dc4923
Brandon Amaya Esquivel <brandon.amaya@sinco.com.co>
2025-11-19 16:20:42 -0500

    ✅ 🔵 Si_CreoTresJugadores_Debe_LanzarExcepcion

--- Acorazados/JuegoAcorazados.cs
+++ Acorazados/JuegoAcorazados.cs
@@ -1,19 +1,32 @@
 using System.Net;
 using Acorazados;
+
 public record AcorazadoAcuatizado(
     Acorazado tipoAcorazado,
     int fila,
     int columna,
     Direccion? direccion
 );
+
 public class JuegoAcorazados
 {
- 
     Jugador jugador1;
-    
+    Jugador jugador2;
+
     public void AgregarJugador(string jugador, List<AcorazadoAcuatizado> acorazados)
     {
-        jugador1 = new Jugador(jugador, acorazados);
+        if (jugador1 is null)
+        {
+            jugador1 = new Jugador(jugador, acorazados);
+        }
+        else if (jugador2 is null)
+        {
+            jugador2 = new Jugador(jugador, acorazados);
+        }
+        else
+        {
+            throw new ArgumentException("No puede agregar mas de dos jugadores");
+        }
     }
 
     public string[,] ImprimirTablero()
@@ -23,7 +36,7 @@ public class JuegoAcorazados
 
     public void Iniciar()
     {
-       if(jugador1 is null)
-           throw new ArgumentNullException("Debe tener jugadores para iniciar el juego");
+        if (jugador1 is null)
+            throw new ArgumentNullException("Debe tener jugadores para iniciar el juego");
     }
 }
