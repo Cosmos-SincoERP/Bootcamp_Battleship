430613792f0913e11a7e5d88af288f9ded71a633
SantiagoRamirezSINCO <santiago.ramirez@sinco.com.co>
2025-11-19 11:10:15 -0500

    🔵 refactor para poder pintar un canonero en cualquier coordenada

--- Acorazados.Test/AcorazadosTest.cs
+++ Acorazados.Test/AcorazadosTest.cs
@@ -12,7 +12,7 @@ public class AcorazadosTest
 
         acorazados.AddPlayer("Santi");
         acorazados.AddPlayer("Ruben");
-        acorazados.Iniciar([],[]);
+        acorazados.Iniciar([], []);
 
         var tablero = acorazados.Imprimir();
         var tableroEsperado = "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |" +
@@ -39,8 +39,8 @@ public class AcorazadosTest
         acorazados.AddPlayer("Ruben");
 
         var canonero = new Nave(1, 1, 1, 1, "g");
-        
-        acorazados.Iniciar([canonero],[]);
+
+        acorazados.Iniciar([canonero], []);
 
         var tablero = acorazados.Imprimir();
         var tableroEsperado = "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |" +
@@ -57,7 +57,7 @@ public class AcorazadosTest
 
         tablero.Should().Be(tableroEsperado);
     }
-    
+
     [Fact]
     public void SiInicioElJuegoConUnCanoneroEn2_3DeJugador1DebeImprimirloBien()
     {
@@ -67,8 +67,8 @@ public class AcorazadosTest
         acorazados.AddPlayer("Ruben");
 
         var canonero = new Nave(2, 3, 2, 3, "g");
-        
-        acorazados.Iniciar([canonero],[]);
+
+        acorazados.Iniciar([canonero], []);
 
         var tablero = acorazados.Imprimir();
         var tableroEsperado = "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |" +
@@ -85,7 +85,7 @@ public class AcorazadosTest
 
         tablero.Should().Be(tableroEsperado);
     }
-    
+
     [Fact]
     public void SiInicioElJuegoConUnCanoneroEn5_5DeJugador1DebeImprimirloBien()
     {
@@ -95,8 +95,8 @@ public class AcorazadosTest
         acorazados.AddPlayer("Ruben");
 
         var canonero = new Nave(5, 5, 5, 5, "g");
-        
-        acorazados.Iniciar([canonero],[]);
+
+        acorazados.Iniciar([canonero], []);
 
         var tablero = acorazados.Imprimir();
         var tableroEsperado = "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |" +
@@ -118,16 +118,19 @@ public class AcorazadosTest
 public class Nave
 {
     public int CoordenadaXInicial { get; set; }
+    public int CoordenadaYInicial { get; set; }
+
     public Nave(int coordenadaXInicial, int coordenadaYInicial, int coordenadaXFinal, int coordenadaYFinal, string tipo)
     {
         CoordenadaXInicial = coordenadaXInicial;
+        CoordenadaYInicial = coordenadaYInicial;
     }
-
 }
 
 public class Battleship
 {
-    private List<string> _tablero = [
+    private List<string> _tablero =
+    [
         "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |",
         "0| | | | | | | | | | |",
         "1| | | | | | | | | | |",
@@ -140,64 +143,20 @@ public class Battleship
         "8| | | | | | | | | | |",
         "9| | | | | | | | | | |"
     ];
-    
+
     public void AddPlayer(string name)
     {
-        
     }
 
     public void Iniciar(List<Nave> navesJugador1, List<Nave> navesJugador2)
     {
         if (navesJugador1.Count != 1) return;
-        
-        if (navesJugador1[0].CoordenadaXInicial == 1)
-        {
-            _tablero = [
-                "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |",
-                "0| | | | | | | | | | |",
-                "1| | g | | | | | | | | |",
-                "2| | | | | | | | | | |",
-                "3| | | | | | | | | | |",
-                "4| | | | | | | | | | |",
-                "5| | | | | | | | | | |",
-                "6| | | | | | | | | | |",
-                "7| | | | | | | | | | |",
-                "8| | | | | | | | | | |",
-                "9| | | | | | | | | | |"
-            ];
-        }
-        else if (navesJugador1[0].CoordenadaXInicial == 2)
-        {
-            _tablero = [
-                "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |",
-                "0| | | | | | | | | | |",
-                "1| | | | | | | | | | |",
-                "2| | | | g | | | | | | |",
-                "3| | | | | | | | | | |",
-                "4| | | | | | | | | | |",
-                "5| | | | | | | | | | |",
-                "6| | | | | | | | | | |",
-                "7| | | | | | | | | | |",
-                "8| | | | | | | | | | |",
-                "9| | | | | | | | | | |"
-            ];
-        }
-        else
-        {
-            _tablero = [
-                "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |",
-                "0| | | | | | | | | | |",
-                "1| | | | | | | | | | |",
-                "2| | | | | | | | | | |",
-                "3| | | | | | | | | | |",
-                "4| | | | | | | | | | |",
-                "5| | | | | | g | | | | |",
-                "6| | | | | | | | | | |",
-                "7| | | | | | | | | | |",
-                "8| | | | | | | | | | |",
-                "9| | | | | | | | | | |"
-            ];
-        }
+
+        var columnas = _tablero[navesJugador1[0].CoordenadaXInicial + 1].Split("|");
+
+        columnas[navesJugador1[0].CoordenadaYInicial + 1] = " g ";
+
+        _tablero[navesJugador1[0].CoordenadaXInicial + 1] = string.Join("|", columnas);
     }
 
     public string Imprimir()
