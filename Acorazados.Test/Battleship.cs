d5a67d9498d61c39cbf8ebe66ae4e71a5a763f98
SantiagoRamirezSINCO <santiago.ramirez@sinco.com.co>
2025-11-19 11:10:59 -0500

    🔵 Extraccion de clases

---
+++ Acorazados.Test/Battleship.cs
@@ -0,0 +1,39 @@
+namespace Acorazados.Test;
+
+public class Battleship
+{
+    private List<string> _tablero =
+    [
+        "| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |",
+        "0| | | | | | | | | | |",
+        "1| | | | | | | | | | |",
+        "2| | | | | | | | | | |",
+        "3| | | | | | | | | | |",
+        "4| | | | | | | | | | |",
+        "5| | | | | | | | | | |",
+        "6| | | | | | | | | | |",
+        "7| | | | | | | | | | |",
+        "8| | | | | | | | | | |",
+        "9| | | | | | | | | | |"
+    ];
+
+    public void AddPlayer(string name)
+    {
+    }
+
+    public void Iniciar(List<Nave> navesJugador1, List<Nave> navesJugador2)
+    {
+        if (navesJugador1.Count != 1) return;
+
+        var columnas = _tablero[navesJugador1[0].CoordenadaXInicial + 1].Split("|");
+
+        columnas[navesJugador1[0].CoordenadaYInicial + 1] = " g ";
+
+        _tablero[navesJugador1[0].CoordenadaXInicial + 1] = string.Join("|", columnas);
+    }
+
+    public string Imprimir()
+    {
+        return string.Join("", _tablero);
+    }
+}
---
+++ Acorazados.Test/Nave.cs
@@ -0,0 +1,13 @@
+namespace Acorazados.Test;
+
+public class Nave
+{
+    public int CoordenadaXInicial { get; set; }
+    public int CoordenadaYInicial { get; set; }
+
+    public Nave(int coordenadaXInicial, int coordenadaYInicial, int coordenadaXFinal, int coordenadaYFinal, string tipo)
+    {
+        CoordenadaXInicial = coordenadaXInicial;
+        CoordenadaYInicial = coordenadaYInicial;
+    }
+}
