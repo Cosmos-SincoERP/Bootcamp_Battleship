1918479a99534892f4bf5fb4c655612011c4cf20
Michael Andrés Lozano Sánchez <Michael.Lozano@sinco.com.co>
2025-11-19 10:32:03 -0500

    🔴Si_SeIniciaElJuegoCon2Jugador_NoDebe_LanzarExcepcionPorCantidadDeJugadores

--- Test.Acorazados/AcorazadosTest.cs
+++ Test.Acorazados/AcorazadosTest.cs
@@ -27,7 +27,7 @@ public class AcorazadosTest
     }
 
     [Fact]
-    public void Si_SeIniciaElJuegoConUnJugador_Debe_LanzarExcepcion()
+    public void Si_SeIniciaElJuegoCon1Jugador_Debe_LanzarExcepcion()
     {
         var juegoAcorazado =  new JuegoAcorazado();
         juegoAcorazado.AgregarJugador();
@@ -36,33 +36,44 @@ public class AcorazadosTest
         
         iniciarJuego.Should().Throw<Exception>().WithMessage("El juego no puede iniciarse hasta que se hayan agregado 2 jugadores");
     }
-    
-    [Fact]
-    public void Si_SeIniciaElJuegoConDosJugadoresElJugador1_Debe_ColocarUnCañoneroEnLaPosicion0_0()
-    {
-        var tamañoTablero = 10;
-        var tablero = new char[tamañoTablero, tamañoTablero];
-        tablero[0, 0] = 'g';
-        var tableroEsperado = string.Empty;
-        for (var x = 0; x < tablero.GetLength(0); x++)
-        {
-            for (int y = 0; y <  tablero.GetLength(1); y++)
-            {
-                tableroEsperado += ' ';
-            }
 
-            tableroEsperado += '\n';
-        }
-        
-        
+    [Fact]
+    public void Si_SeIniciaElJuegoCon2Jugador_NoDebe_LanzarExcepcionPorCantidadDeJugadores()
+    {
         var juegoAcorazado =  new JuegoAcorazado();
         juegoAcorazado.AgregarJugador();
-        juegoAcorazado.AgregarJugador();
 
-        juegoAcorazado.Iniciar();
+        var iniciarJuego = () => juegoAcorazado.Iniciar();
         
-        juegoAcorazado.Imprimir().Should().Be(tableroEsperado);
+        iniciarJuego.Should().NotThrow();
     }
+    
+    // [Fact]
+    // public void Si_SeIniciaElJuegoConDosJugadoresElJugador1_Debe_ColocarUnCañoneroEnLaPosicion0_0()
+    // {
+    //     var tamañoTablero = 10;
+    //     var tablero = new char[tamañoTablero, tamañoTablero];
+    //     tablero[0, 0] = 'g';
+    //     var tableroEsperado = string.Empty;
+    //     for (var x = 0; x < tablero.GetLength(0); x++)
+    //     {
+    //         for (int y = 0; y <  tablero.GetLength(1); y++)
+    //         {
+    //             tableroEsperado += ' ';
+    //         }
+    //
+    //         tableroEsperado += '\n';
+    //     }
+    //     
+    //     
+    //     var juegoAcorazado =  new JuegoAcorazado();
+    //     juegoAcorazado.AgregarJugador();
+    //     juegoAcorazado.AgregarJugador();
+    //
+    //     juegoAcorazado.Iniciar();
+    //     
+    //     juegoAcorazado.Imprimir().Should().Be(tableroEsperado);
+    // }
 }
 
 public class JuegoAcorazado
@@ -80,7 +91,8 @@ public class JuegoAcorazado
 
     public void Iniciar()
     {
-        throw new Exception("El juego no puede iniciarse hasta que se hayan agregado 2 jugadores");
+        if(_jugadores.Count != 2)
+            throw new Exception("El juego no puede iniciarse hasta que se hayan agregado 2 jugadores");
     }
 
     public object Imprimir()
