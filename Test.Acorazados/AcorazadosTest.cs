b34a63059bb976125f4a229004213fda7f4b4a47
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-21 09:00:33 -0500

    🔴 Si_NoHayJugadoresYSeIniciaElJuego_Debe_LanzarUnaExcepcionPorCantidadDeJugadores

--- Test.Acorazados/AcorazadosTest.cs
+++ Test.Acorazados/AcorazadosTest.cs
@@ -4,11 +4,22 @@ namespace Test.BattleShip;
 
 public class AcorazadosTest
 {
+    [Fact]
+    public void Si_NoHayJugadoresYSeIniciaElJuego_Debe_LanzarUnaExcepcionPorCantidadDeJugadores()
+    {
+        var juegoAcorazado = new JuegoAcorazados();
+        var iniciar = () => juegoAcorazado.Iniciar();
 
-   
+        iniciar.Should().Throw<Exception>()
+            .WithMessage("No se puede iniciar el juego, debe haber al menos 2 jugadores");
+    }
+
 }
 
 public class JuegoAcorazados
 {
-   
+    public void Iniciar()
+    {
+        throw new NotImplementedException();
+    }
 }
