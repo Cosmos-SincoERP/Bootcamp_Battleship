159d108eb5cfc0f84f324f172b508cb2679871ae
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-21 09:18:28 -0500

    🔴 Si_TresJugadoreYSeIniciaElJuego_Debe_LanzarUnaExcepcionPorCantidadDeJugadores

--- Test.Acorazados/AcorazadosTest.cs
+++ Test.Acorazados/AcorazadosTest.cs
@@ -18,7 +18,20 @@ public class AcorazadosTest
     public void Si_HaySoloUnJugadoreYSeIniciaElJuego_Debe_LanzarUnaExcepcionPorCantidadDeJugadores()
     {
         var juegoAcorazado = new JuegoAcorazados();
-        juegoAcorazado.AgregarJugador("Diego");
+        juegoAcorazado.AgregarJugador("Jugador 1");
+        var iniciar = () => juegoAcorazado.Iniciar();
+
+        iniciar.Should().Throw<Exception>()
+            .WithMessage("No se puede iniciar el juego, debe haber al menos 2 jugadores");
+    }
+
+    [Fact]
+    public void Si_TresJugadoreYSeIniciaElJuego_Debe_LanzarUnaExcepcionPorCantidadDeJugadores()
+    {
+        var juegoAcorazado = new JuegoAcorazados();
+        juegoAcorazado.AgregarJugador("Jugador 1");
+        juegoAcorazado.AgregarJugador("Jugador 2");
+        juegoAcorazado.AgregarJugador("Jugador 3");
         var iniciar = () => juegoAcorazado.Iniciar();
 
         iniciar.Should().Throw<Exception>()
