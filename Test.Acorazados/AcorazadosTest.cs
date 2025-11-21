6e370d35c4535cafdce7b0d6e3c59ec1ba7be88b
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-21 11:42:23 -0500

    🔴 Si_SeIniciaJuegoYElJugador1PosicionaElPortavionesHorizontalDesdeLaPosicion00_Debe_ImprimirTableroDelJugador1ConElPortavionesPosicionadoDesde00Hasta03

--- Test.Acorazados/AcorazadosTest.cs
+++ Test.Acorazados/AcorazadosTest.cs
@@ -8,7 +8,7 @@ public class AcorazadosTest
     public void Si_NoHayJugadoresYSeIniciaElJuego_Debe_LanzarUnaExcepcionPorCantidadDeJugadores()
     {
         var juegoAcorazado = new JuegoAcorazados();
-        var iniciar = () => juegoAcorazado.Iniciar();
+        var iniciar = () => juegoAcorazado.Iniciar([]);
 
         iniciar.Should().Throw<Exception>()
             .WithMessage("No se puede iniciar el juego, debe haber al menos 2 jugadores");
@@ -19,7 +19,7 @@ public class AcorazadosTest
     {
         var juegoAcorazado = new JuegoAcorazados();
         juegoAcorazado.AgregarJugador("Jugador 1");
-        var iniciar = () => juegoAcorazado.Iniciar();
+        var iniciar = () => juegoAcorazado.Iniciar([]);
 
         iniciar.Should().Throw<Exception>()
             .WithMessage("No se puede iniciar el juego, debe haber al menos 2 jugadores");
@@ -32,7 +32,7 @@ public class AcorazadosTest
         juegoAcorazado.AgregarJugador("Jugador 1");
         juegoAcorazado.AgregarJugador("Jugador 2");
         juegoAcorazado.AgregarJugador("Jugador 3");
-        var iniciar = () => juegoAcorazado.Iniciar();
+        var iniciar = () => juegoAcorazado.Iniciar([]);
 
         iniciar.Should().Throw<Exception>()
             .WithMessage("No se puede iniciar el juego, debe haber al menos 2 jugadores");
@@ -44,7 +44,7 @@ public class AcorazadosTest
         var juegoAcorazado = new JuegoAcorazados();
         juegoAcorazado.AgregarJugador("Jugador 1");
         juegoAcorazado.AgregarJugador("Jugador 2");
-        juegoAcorazado.Iniciar();
+        juegoAcorazado.Iniciar([]);
 
         string tableroEsperado = "\n" +
                          "   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | \n" +
@@ -71,12 +71,47 @@ public class AcorazadosTest
 
     }
 
+    [Fact]
+    public void Si_SeIniciaJuegoYElJugador1PosicionaElPortavionesHorizontalDesdeLaPosicion00_Debe_ImprimirTableroDelJugador1ConElPortavionesPosicionadoDesde00Hasta03()
+    {
+        var juegoAcorazado = new JuegoAcorazados();
+        juegoAcorazado.AgregarJugador("Jugador 1");
+        juegoAcorazado.AgregarJugador("Jugador 2");
+
+        List<(int x, int y, char barco, int cantidadPosiciones)> barcosJugador1 = new()
+        {
+            (0, 0, 'c',4)
+        };
+
+        juegoAcorazado.Iniciar(barcosJugador1);
+
+        string tableroEsperado = "\n" +
+                         "   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | \n" +
+                         "-------------------------------------------| \n" +
+                         " 0 | c | c | c | c |   |   |   |   |   |   | \n" +
+                         " 1 |   |   |   |   |   |   |   |   |   |   | \n" +
+                         " 2 |   |   |   |   |   |   |   |   |   |   | \n" +
+                         " 3 |   |   |   |   |   |   |   |   |   |   | \n" +
+                         " 4 |   |   |   |   |   |   |   |   |   |   | \n" +
+                         " 5 |   |   |   |   |   |   |   |   |   |   | \n" +
+                         " 6 |   |   |   |   |   |   |   |   |   |   | \n" +
+                         " 7 |   |   |   |   |   |   |   |   |   |   | \n" +
+                         " 8 |   |   |   |   |   |   |   |   |   |   | \n" +
+                         " 9 |   |   |   |   |   |   |   |   |   |   | \n" +
+                         "-------------------------------------------| \n";
+
+        string tableroJugador1 = juegoAcorazado.Imprimir("Jugador 1");
+
+        tableroJugador1.Should().Be(tableroEsperado);
+    }
+
+
 }
 
 public class JuegoAcorazados
 {
     public List<(string, char[,])> _jugadores { get; } = new();
-    public void Iniciar()
+    public void Iniciar(List<(int x, int y, char barco, int cantidadPosiciones)> barcosJugador1)
     {
         if (_jugadores.Count != 2)
             throw new Exception("No se puede iniciar el juego, debe haber al menos 2 jugadores");
