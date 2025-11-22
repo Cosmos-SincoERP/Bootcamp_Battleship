f5cdf7eb9a381203f8d0fceb003872dfb53ffd4a
Arnod Chiriví <arnod.chirivi@sinco.com.co>
2025-11-22 16:55:42 -0500

    🟥 Si_SeIntentaDisparar_AntesDeQueElJuegoSeEncuentreEnCurso_DebeLanzarUnaExcepcion

--- Acorazados.Tests/AcorazadosTests.cs
+++ Acorazados.Tests/AcorazadosTests.cs
@@ -664,9 +664,37 @@ public class AcorazadosTests
 
         var caller = () => acorazados.IniciarJuego();
 
-
         caller.Should().ThrowExactly<InvalidOperationException>()
             .WithMessage(
                 "No se puede iniciar el juego hasta que todos los jugadores hayan posicionado su flota completa");
+    }
+
+    [Fact]
+    public void Si_SeIntentaDisparar_AntesDeQueElJuegoSeEncuentreEnCurso_DebeLanzarUnaExcepcion()
+    {
+        var acorazados = new Acorazados();
+        acorazados.AgregarJugador("jugador 1");
+        acorazados.AgregarJugador("jugador 2");
+        
+        var jugador1= acorazados.BuscarJugador("jugador 1");
+        jugador1.AgregarCarrier(0, 0, Orientacion.Horizontal);
+        jugador1.AgregarDestroyer(1, 0, Orientacion.Horizontal);
+        jugador1.AgregarDestroyer(2, 0, Orientacion.Horizontal);    
+        jugador1.AgregarGunShip(3, 0);
+        jugador1.AgregarGunShip(4, 0);
+        jugador1.AgregarGunShip(8, 0);
+        
+        var jugador2 = acorazados.BuscarJugador("jugador 2");
+        jugador2.AgregarCarrier(0, 0, Orientacion.Horizontal);
+        jugador2.AgregarDestroyer(1, 0, Orientacion.Horizontal);
+        jugador2.AgregarDestroyer(2, 0, Orientacion.Horizontal);
+        jugador2.AgregarGunShip(3, 0); 
+        jugador2.AgregarGunShip(4, 0); 
+        jugador2.AgregarGunShip(5, 0); 
+        jugador2.AgregarGunShip(6, 0);
+
+        var caller = () => acorazados.Disparar(0, 0);
+
+        caller.Should().ThrowExactly<InvalidOperationException>().WithMessage("El juego no ha comenzado");
     }
 }
