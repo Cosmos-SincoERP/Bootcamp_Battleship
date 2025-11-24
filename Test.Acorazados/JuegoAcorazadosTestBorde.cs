8cea998a02a733e5a2d712078d3f6566cfc0242b
Diego Rubio Cortes <diego.rubio@sinco.com.co>
2025-11-24 07:44:25 -0500

    🔴 Si_IniciaElJuegoYSePosicionanBarcoEnUnaCoordenadaEnYMayorA10_Debe_LanzarExcepcion

--- Test.Acorazados/JuegoAcorazadosTestBorde.cs
+++ Test.Acorazados/JuegoAcorazadosTestBorde.cs
@@ -8,9 +8,6 @@ public class JuegoAcorazadosTestBorde
     public void
     Si_IniciaElJuegoYSePosicionanBarcoEnUnaCoordenadaEnXMayorA10_Debe_LanzarExcepcion()
     {
-        var tablero = new char[10, 10];
-        tablero[0, 0] = 'o';
-        var tableroEsperado = Mocks.TableroEsperado(tablero);
         var juegoAcorazado = () => Mocks.MockIniciarJuegoConBarcoEnCoordenadaXMayorA10();
 
         juegoAcorazado.Should().ThrowExactly<Exception>().WithMessage("El jugador 1, ha enviado un barco con coordenadas invalidas (11,1)");
@@ -19,11 +16,17 @@ public class JuegoAcorazadosTestBorde
     [Fact]
     public void Si_IniciaElJuegoYSePosicionanBarcoEnUnaCoordenadaEnXMenoA0_Debe_LanzarExcepcion()
     {
-        var tablero = new char[10, 10];
-        tablero[0, 0] = 'o';
-        var tableroEsperado = Mocks.TableroEsperado(tablero);
         var juegoAcorazado = () => Mocks.MockIniciarJuegoConBarcoEnCoordenadaXMenorA0();
 
         juegoAcorazado.Should().ThrowExactly<Exception>().WithMessage("El jugador 1, ha enviado un barco con coordenadas invalidas (-1,1)");
+    }
+
+
+    [Fact]
+    public void Si_IniciaElJuegoYSePosicionanBarcoEnUnaCoordenadaEnYMayorA10_Debe_LanzarExcepcion()
+    {
+        var juegoAcorazado = () => Mocks.MockIniciarJuegoConBarcoEnCoordenadaYMayorA10();
+
+        juegoAcorazado.Should().ThrowExactly<Exception>().WithMessage("El jugador 1, ha enviado un barco con coordenadas invalidas (11,1)");
     }
 }
