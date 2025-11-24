f720292a483ddcc4732b2ab38823085faa7ac6b2
david.cubides <david.cubides@sinco.com.co>
2025-11-24 11:59:11 -0500

    🔴Si_SeJuegaUnaPartidaCompletaConMultiplesBarcosYDisparos_Debe_ImprimirReporteYTableroFinal

--- Acorazados.Test/obj/Debug/net9.0/Acorazados.Test.AssemblyInfo.cs
+++ Acorazados.Test/obj/Debug/net9.0/Acorazados.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Acorazados.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e99dfb3c8909fd48537e5f077428667cf23438db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da9736ebafa99f37df6e30516ba16af39140b15")]
 [assembly: System.Reflection.AssemblyProductAttribute("Acorazados.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Acorazados.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
